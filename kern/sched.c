d993ee6053c7a4fe139ebb8d20b29d440ee8aa29
mghera02 <mghera02@hotmail.com>
2024-03-12 21:54:23 -0400

    exercise 13 done (pretty sure this purposely breaks part b)

--- kern/sched.c
+++ kern/sched.c
@@ -104,9 +104,7 @@ sched_halt(void)
 		"movl %0, %%esp\n"
 		"pushl $0\n"
 		"pushl $0\n"
-        // LAB 4:
-		// Uncomment the following line after completing exercise 13
-		//"sti\n"
+		"sti\n"
 		"1:\n"
 		"hlt\n"
 		"jmp 1b\n"
